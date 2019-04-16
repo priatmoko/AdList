ee12575a5b2ae1e659531ab6b8c01f565e8fe76f
Priatmoko <priatmoko@orix.co.id>
2019-04-16 14:41:06 +0700

    Change display

--- App.js
+++ App.js
@@ -1,37 +1,54 @@
 import React from 'react';
-import { StyleSheet,FlatList, Text, TextInput, View, Button, KeyboardAvoidingView } from 'react-native';
+import {TouchableOpacity, TextInput, StyleSheet, Text, View, Alert, FlatList } from 'react-native';
+import {Constants } from 'expo';
 
 export default class App extends React.Component {
+    
     constructor(props){
         super(props);
-        this.state = {textInput : '', data : []};
+        this.state = {barcode : '', data : []};
+        this.buttonPress = this.buttonPress.bind(this);
     }
+    
+    buttonPress(){
+        if (this.state.barcode!=''){
+            this.setState((prevData)=>({
+                data : prevData.data.concat({key : prevData.data.length==0 || prevData.data.length==1 ?prevData.data.length+'':(prevData.data.length+1)+'', desc:prevData.barcode})
+            }));
+        }else{
+            Alert.alert('Please type word!');
+        }
+        this.setState((barcode)=>({barcode:''}));
+    }
+
     render() {
         return (
-            <View style={styles.container}>
-                <View style={styles.formContainer}>
-                    <View style={{justifyContent:'center', alignItems:'center'}}>
-                        <Text style={styles.label}>AdList</Text>
-                    </View>
-                    <TextInput placeholder="Type list here" style={styles.text} onChangeText={(textInput)=>{this.setState({textInput})}} value={this.state.textInput}/>
-                    <Button color="#fc2020" title="Add" onPress={()=>{
-                        // let newData = new Array;
-                        // newData.push({key : this.state.textInput});
-                        this.setState((prevData)=>({data : 
-                                prevData.data.concat({key : prevData.data.length==0 || prevData.data.length==1 ?prevData.data.length+'':(prevData.data.length+1)+'', desc:prevData.textInput})
-                            }));
-                        this.setState((textInput)=>({textInput:''}));    
-                    }} />
-                </View>    
-                <View style={styles.listContainer}>
-                    <KeyboardAvoidingView behavior='padding' enabled>    
+            <View  style={{flex:1}}>
+                <View style={styles.statusBar}/>
+                <View style={styles.container}>
+                    <View style={styles.formContainer}>
+                        <Text style={styles.label}>Scan Code</Text>
+                        <TextInput placeholder='Type Code here' 
+                            onChangeText={(barcode)=>(this.setState({barcode}))}
+                            textAlign = 'center' 
+                            value={this.state.barcode}
+                            style={styles.textInput}/>
+                        <TouchableOpacity 
+                            style={styles.btnContainer}
+                            onPress={this.buttonPress}>
+                            <Text style={{color :'#ffffff'}}>Press me</Text>
+                        </TouchableOpacity>    
+                    </View>        
+                    <View style={styles.flatContainer}>
                         <FlatList 
-                            data={this.state.data}
+                            data = {this.state.data}
                             renderItem = {({item})=>{
-                                return (<Text style={styles.item}>{item.desc}</Text>);
+                                return (
+                                    <Text style={styles.item}>{item.desc}</Text>
+                                )
                             }}
                         />
-                    </KeyboardAvoidingView>    
+                    </View>        
                 </View>
             </View>
         );
@@ -41,17 +58,62 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: 22
+        backgroundColor: '#fff',
+        marginTop : 5
     },
     formContainer : {
         flex : 1,
-        padding : 5
+        alignItems : 'center'
     },
-    listContainer : {
-        flex : 4,
-        backgroundColor : '#cccccc'
+    flatContainer : {
+        flex : 3,
+        backgroundColor : '#f9f4f4'
     },
-    text : {padding : 5, height : 50, fontSize:15},
-    label : {paddingLeft : 5, height : 20, fontSize:18},
-    item : {padding:10,fontSize:15, backgroundColor: '#f9dbdb', marginTop:5}
+    label : {
+        fontSize : 20,
+        color : '#680909',
+        marginTop : 12
+    },
+    statusBar: {
+        backgroundColor: "#C2185B",
+        height: Constants.statusBarHeight,
+    },
+    textInput : {
+        fontSize : 14,
+        borderWidth : 1,
+        borderColor : '#cccccc',
+        alignSelf :'stretch',
+        margin : 5,
+        marginTop : 10,
+        marginBottom : 10,
+        padding : 7,
+        paddingLeft : 12,
+        paddingRight : 12,
+        borderRadius : 20
+    },
+    btnContainer : {
+        padding : 10,
+        paddingLeft : 20,
+        paddingRight : 20,
+        backgroundColor:'#C2185B',
+        borderRadius:20,
+        borderWidth: 1,
+        borderColor: '#fff'
+    },
+    item : {
+        backgroundColor : '#ffffff',
+        color : '#000000',
+        padding : 10,
+        marginLeft:5,
+        marginRight:5,
+        marginTop : 5,
+        borderWidth : 1,
+        borderColor : '#e2e0e0',
+        borderRadius : 5,
+        elevation: 1,
+        shadowColor: '#000',
+        shadowRadius : 1,
+        shadowOffset: { width: 5, height: -5 },
+        shadowOpacity: 0.1,
+    }
 });
